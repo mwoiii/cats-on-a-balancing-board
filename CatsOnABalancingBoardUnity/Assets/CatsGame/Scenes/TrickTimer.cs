1a938a8ccefbb1011477126cc18e7bfe4594c55e
nod03 <159582581+nod03@users.noreply.github.com>
2026-07-26 15:26:08 +0100

    trick countdown sound

--- CatsOnABalancingBoardUnity/Assets/CatsGame/Scenes/TrickTimer.cs
+++ CatsOnABalancingBoardUnity/Assets/CatsGame/Scenes/TrickTimer.cs
@@ -6,6 +6,16 @@ public class TrickTimer : MonoBehaviour {
     public int trickLength;
     public int initialBonus;
 
+    public AudioClip countdownSound;
+    public AudioClip completeSound;
+
+    public AudioSource countdownSource;
+    public AudioSource completeSource;
+
+    public float volume = 0.7f;
+    public float basePitch = 1f;
+    public float pitchStep = 0.1f;
+    public float maxPitch = 2f;
 
     void Awake()
     {
@@ -15,6 +25,9 @@ public class TrickTimer : MonoBehaviour {
     void Start() {
         StartCoroutine(Timer());
         CatManagerScript.LostCat += ResetTimerAndCombo;
+
+        countdownSource.volume = volume / 1.5f;
+        completeSource.volume = volume;
     }
 
     public static int currentSecond { get; private set; }
@@ -30,7 +43,13 @@ public class TrickTimer : MonoBehaviour {
             yield return new WaitForSeconds(1);
             currentSecond--;
             onTimerChanged?.Invoke(currentSecond);
+            if (currentSecond == 3)
+            {
+                PlayCountdown();
+            }
             if (currentSecond == 0) {
+                PlayComplete();
+
                 StartCoroutine(CatSpawnerScript.instance.PopulateBoard(litterBonus));
                 litterBonus *= 2;
                 onLitterBonusChanged?.Invoke(litterBonus);
@@ -44,9 +63,29 @@ public class TrickTimer : MonoBehaviour {
         litterBonus = initialBonus;
         onTimerChanged?.Invoke(currentSecond);
         onLitterBonusChanged?.Invoke(litterBonus);
+
+        countdownSource.Stop();
+        completeSource.pitch = basePitch;
     }
 
     void OnDestroy() {
         CatManagerScript.LostCat -= ResetTimerAndCombo;
+    }
+
+    void PlayCountdown()
+    {
+        if (countdownSound != null && countdownSource != null)
+        {
+            countdownSource.PlayOneShot(countdownSound);
+        }
+    }
+
+    void PlayComplete()
+    {
+        if (completeSound != null && completeSource != null)
+        {
+            completeSource.PlayOneShot(completeSound);
+            completeSource.pitch = Mathf.Min(completeSource.pitch + pitchStep, maxPitch);
+        }
     }
 }
