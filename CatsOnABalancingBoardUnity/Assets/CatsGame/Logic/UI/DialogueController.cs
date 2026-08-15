7135b0b570d93d02b92157e9b67a96d53b9b74d2
mwoiii <147194899+mwoiii@users.noreply.github.com>
2026-08-15 15:48:50 +0100

    kill text render textures in favour of shader
    and start dialoguecontroller....

---
+++ CatsOnABalancingBoardUnity/Assets/CatsGame/Logic/UI/DialogueController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace OMC.UI {
+    public class DialogueController : MonoBehaviour {
+        [SerializeField]
+        private TextMeshProUGUI textInput;
+
+        public float defaultDelay = 0.05f;
+
+        private void OnEnable() {
+            if (!textInput) {
+                textInput = GetComponent<TextMeshProUGUI>();
+            }
+
+            if (textInput) {
+                textInput.maxVisibleCharacters = 0;
+                StartCoroutine(WriteText());
+            }
+        }
+
+        private IEnumerator WriteText() {
+            for (int i = 0; i < textInput.text.Length; i++) {
+                if (textInput) {
+                    textInput.maxVisibleCharacters = i + 1;
+                    yield return new WaitForSeconds(defaultDelay);
+                }
+            }
+        }
+    }
+}
