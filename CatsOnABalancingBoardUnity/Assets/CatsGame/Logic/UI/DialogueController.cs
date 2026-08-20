2a6b514e0fda564cdf01886a6d441d4f0d0bac19
mwoiii <147194899+mwoiii@users.noreply.github.com>
2026-08-20 01:41:13 +0100

    new shake type and all styles variable
    little bit all over the place some redundancies here and there but It works!!! it's worging.

--- CatsOnABalancingBoardUnity/Assets/CatsGame/Logic/UI/DialogueController.cs
+++ CatsOnABalancingBoardUnity/Assets/CatsGame/Logic/UI/DialogueController.cs
@@ -28,20 +28,21 @@ namespace OMC.UI {
 
         private Vector3[] animatedVertices;
 
-        private List<int> shakeVertices = new List<int>();
-        private List<int> shakeBreaks = new List<int>();
+        private Dictionary<int, int> shakeCharacters = new Dictionary<int, int>(); // start vert -> value
 
-        private Dictionary<int, int> pauseCharacters = new Dictionary<int, int>();
+        private Dictionary<int, int> shakeGroupVertices = new Dictionary<int, int>(); // start vert -> value
+        private List<int> shakeGroupBreaks = new List<int>();
 
-        private static Regex startRegex = new Regex("<style=\"(\\w*)\">");
+        private Dictionary<int, int> pauseCharacters = new Dictionary<int, int>(); // character index to stop before -> value
+
+        private static Regex startRegex = new Regex("<style=\"(\\D*)(\\d)\">");
         private static Regex endRegex = new Regex("</style>");
-
-        private const float PauseDelay = 1f;
 
         private struct StyleInfo {
             public bool isStart;
             public int index;
             public string styleName;
+            public int styleValue;
         }
 
         private void OnEnable() {
@@ -61,45 +62,16 @@ namespace OMC.UI {
             }
         }
 
-        private void ResetDialogue() {
-            textInput.maxVisibleCharacters = 0;
-            visibleCharacters = 0;
-            animateCooldown = 0f;
-            writeCooldown = 0f;
-        }
-
         private void Update() {
             UpdateWriteText();
             UpdateAnimateText();
         }
 
-        private void UpdateWriteText() {
-            if (visibleCharacters < maxVisibleCharacters) {
-                writeCooldown -= Time.deltaTime;
-                if (writeCooldown <= 0) {
-                    visibleCharacters++;
-                    textInput.maxVisibleCharacters = visibleCharacters;
-                    if (pauseCharacters.TryGetValue(visibleCharacters, out int pauseValue)) {
-                        writeCooldown = PauseDelay * pauseValue;
-                    } else {
-                        writeCooldown = writeDelay;
-                    }
-                }
-            }
-        }
-
-        private void UpdateAnimateText() {
-            animateCooldown -= Time.deltaTime;
-            if (animateCooldown <= 0) {
-                textInput.ForceMeshUpdate(false, false);
-                animatedVertices = textInput.textInfo.meshInfo[0].vertices;
-                int lastVisible = visibleCharacters * 4 - 1;
-                ApplyShake(animatedVertices, lastVisible);
-                animateCooldown = animationDelay;
-            }
-
-            textInput.textInfo.meshInfo[0].vertices = animatedVertices;
-            textInput.UpdateVertexData(TMP_VertexDataUpdateFlags.Vertices);
+        private void ResetDialogue() {
+            textInput.maxVisibleCharacters = 0;
+            visibleCharacters = 0;
+            animateCooldown = 0f;
+            writeCooldown = 0f;
         }
 
         private void ParseText() {
@@ -112,12 +84,13 @@ namespace OMC.UI {
                 styleQueue.Enqueue(new StyleInfo {
                     isStart = match.Groups.Count > 1,
                     styleName = match.Groups[1].Value.ToLower(),
+                    styleValue = math.max(0, GetScaledValue(int.TryParse(match.Groups[2].Value, out int value) ? value : 0)),
                     index = match.Index
                 });
             }
 
             // helpers
-            Stack<string> styleStack = new Stack<string>();
+            Stack<StyleInfo> styleStack = new Stack<StyleInfo>();
             Dictionary<string, int> styleActive = new Dictionary<string, int>();
             HashSet<string> styleBreaks = new HashSet<string>();
             int characterIndex = -1;
@@ -137,33 +110,43 @@ namespace OMC.UI {
                         styleQueue.Dequeue();
                         if (result.isStart) {
                             styleActive.TryGetValue(result.styleName, out int active);
-                            styleActive[result.styleName] = active + 1;
-                            styleStack.Push(result.styleName);
+                            styleActive[result.styleName] = active + result.styleValue;
+                            styleStack.Push(result);
                         } else {
                             if (styleStack.Count <= 0) {
                                 continue;
                             }
-                            string styleName = styleStack.Pop();
-                            styleActive.TryGetValue(styleName, out int active);
-                            if (active == 1) {
-                                styleBreaks.Add(styleName);
+                            StyleInfo styleToRemove = styleStack.Pop();
+                            if (styleActive.TryGetValue(styleToRemove.styleName, out int active)) {
+                                styleActive[styleToRemove.styleName] = math.max(0, active - styleToRemove.styleValue);
+                                if (active > 0 && styleActive[styleToRemove.styleName] == 0) {
+                                    styleBreaks.Add(styleToRemove.styleName);
+                                }
                             }
-                            styleActive[styleName] = math.max(active - 1, 0);
                         }
                     } else {
                         break;
                     }
                 }
 
+
                 // handling the individual style cases depending on if they are active or not
-                if (styleActive.TryGetValue("shake", out int shakeValue) && shakeValue > 0) {
-                    AddVertices(shakeVertices, character.vertexIndex);
+
+                const string shakeGroupPrefix = "shakegroup";
+                if (styleActive.TryGetValue(shakeGroupPrefix, out int shakeGroupValue) && shakeGroupValue > 0) {
+                    shakeGroupVertices[character.vertexIndex] = shakeGroupValue;
                 }
-                if (styleBreaks.Contains("shake")) {
-                    shakeBreaks.Add(character.vertexIndex - 1);
+                if (styleBreaks.Contains(shakeGroupPrefix)) {
+                    shakeGroupBreaks.Add(character.vertexIndex - 1);
                 }
 
-                if (styleActive.TryGetValue("pause", out int pauseValue) && pauseValue > 0) {
+                const string shakePrefix = "shake";
+                if (styleActive.TryGetValue(shakePrefix, out int shakeValue) && shakeValue > 0) {
+                    shakeCharacters[character.vertexIndex] = shakeValue;
+                }
+
+                const string pausePrefix = "pause";
+                if (styleActive.TryGetValue(pausePrefix, out int pauseValue) && pauseValue > 0) {
                     pauseCharacters[characterIndex + 1] = pauseValue;
                 }
             }
@@ -172,33 +155,77 @@ namespace OMC.UI {
             parsed = true;
         }
 
+        private void UpdateWriteText() {
+            if (visibleCharacters < maxVisibleCharacters) {
+                writeCooldown -= Time.deltaTime;
+                if (writeCooldown <= 0) {
+                    visibleCharacters++;
+                    textInput.maxVisibleCharacters = visibleCharacters;
+                    if (pauseCharacters.TryGetValue(visibleCharacters, out int pauseValue)) {
+                        writeCooldown = 0.3333f * pauseValue;
+                    } else {
+                        writeCooldown = writeDelay;
+                    }
+                }
+            }
+        }
+
+        private void UpdateAnimateText() {
+            animateCooldown -= Time.deltaTime;
+            if (animateCooldown <= 0) {
+                textInput.ForceMeshUpdate(false, false);
+                animatedVertices = textInput.textInfo.meshInfo[0].vertices;
+                int lastVertex = visibleCharacters * 4 - 1;
+                ApplyShake(animatedVertices, lastVertex);
+                ApplyShakeGroup(animatedVertices, lastVertex);
+                animateCooldown = animationDelay;
+            }
+
+            textInput.textInfo.meshInfo[0].vertices = animatedVertices;
+            textInput.UpdateVertexData(TMP_VertexDataUpdateFlags.Vertices);
+        }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void ApplyShake(Vector3[] vertices, int lastVisible) {
+        private void ApplyShakeGroup(Vector3[] vertices, int lastVertex) {
             int nextBreak = 0;
-            Vector3 randomShake = GetRandomShake();
-            foreach (int vertex in shakeVertices) {
-                if (vertex > lastVisible) {
+            Vector3 randomShake = GetRandomShake(shakeGroupVertices.Values.First() * 4f);
+            foreach (int vertex in shakeGroupVertices.Keys) {
+                if (vertex > lastVertex) {
                     break;
                 }
-                if (nextBreak < shakeBreaks.Count && vertex > shakeBreaks[nextBreak]) {
-                    randomShake = GetRandomShake();
+                if (nextBreak < shakeGroupBreaks.Count && vertex > shakeGroupBreaks[nextBreak]) {
+                    randomShake = GetRandomShake(shakeGroupVertices[vertex] * 4f);
                     nextBreak++;
                 }
                 vertices[vertex] += randomShake;
+                vertices[vertex + 1] += randomShake;
+                vertices[vertex + 2] += randomShake;
+                vertices[vertex + 3] += randomShake;
             }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void AddVertices(List<int> vertices, int start) {
-            vertices.Add(start);
-            vertices.Add(start + 1);
-            vertices.Add(start + 2);
-            vertices.Add(start + 3);
+        private void ApplyShake(Vector3[] vertices, int lastVertex) {
+            foreach (int vertex in shakeCharacters.Keys) {
+                if (vertex >= lastVertex) {
+                    break;
+                }
+                Vector3 randomShake = GetRandomShake(shakeCharacters[vertex] * 4f);
+                vertices[vertex] += randomShake;
+                vertices[vertex + 1] += randomShake;
+                vertices[vertex + 2] += randomShake;
+                vertices[vertex + 3] += randomShake;
+            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private Vector3 GetRandomShake() {
-            return Vector3.up * UnityEngine.Random.Range(-20f, 20f) + Vector3.right * UnityEngine.Random.Range(-20f, 20f);
+        private int GetScaledValue(int value) {
+            return math.max(0, (value * 2) - 1);  // goes up as 1, 3, 5, etc. can combine tags to reach values in between
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private Vector3 GetRandomShake(float intensity) {
+            return (Vector3.up * UnityEngine.Random.Range(-1f, 1f) + Vector3.right * UnityEngine.Random.Range(-1f, 1f)) * intensity;
         }
     }
 }
